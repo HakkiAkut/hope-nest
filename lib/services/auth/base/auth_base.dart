6814ed7bd9193d96b34e7a4a77769a4f8c7b12b5
HakkiAkut <hakut37@gmail.com>
2021-11-17 21:07:47 +0300

    services :: auth : firebase auth service created

--- lib/services/auth/base/auth_base.dart
+++ lib/services/auth/base/auth_base.dart
@@ -2,7 +2,7 @@ import 'package:hope_nest/models/app_user.dart';
 
 /// Base class for Authentication operations
 abstract class AuthMethods{
-  Future<AppUser> currentUser();
-  Future<AppUser> signInWithPhoneNumber({required String phone, required String pwd});
+  Future<AppUser?> currentUser();
+  Future<AppUser> signInWithPhoneNumber({required String phone});
   Future<bool> signOut();
 }
---
+++ lib/services/auth/firebase/auth.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hope_nest/models/app_user.dart';
+import 'package:hope_nest/services/auth/base/auth_base.dart';
+
+class AuthService implements AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  /// Firebase current user
+  @override
+  Future<AppUser?> currentUser() async {
+    User? user = _auth.currentUser;
+
+    return user != null ? convertUserToAppUser(user) : null;
+  }
+
+  /// Firebase sign out
+  @override
+  Future<bool> signOut() async {
+    await _auth.signOut().onError((error, stackTrace) => false);
+    return true;
+  }
+
+  /// converts Firebase user to Application User
+  AppUser convertUserToAppUser(User user) {
+    return AppUser(uid: user.uid, email: user.phoneNumber);
+  }
+
+  @override
+  Future<AppUser> signInWithPhoneNumber({required String phone}) async{
+    return await AppUser(uid: "123");
+  }
+}
