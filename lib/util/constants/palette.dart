82b3f022ce38b02a47d9b59964001b1136b4a1c5
HakkiAkut <hakut37@gmail.com>
2021-11-17 20:17:17 +0300

    util :: palette created

---
+++ lib/util/constants/palette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const MAIN_COLOR = Colors.orange;
+  static const BACKGROUND = Colors.white;
+  static const BOLD_COLOR = Color(0xFFE65100);
+  static const GRADIENT_COLOR = [Colors.deepOrangeAccent, Colors.deepOrange];
+  static const PROFILE_GRADIENT = [Colors.white, Colors.grey];
+  static const TEXT_COLOR = Colors.black;
+}
