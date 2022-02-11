19c27c7568e9ba3a7023e872474e5bfd0e6c6b5d
HakkiAkut <hakut37@gmail.com>
2022-02-11 21:51:24 +0300

    VM :: AdvertVM : created and AdvertMethods implemented

---
+++ lib/view_models/advert_vm.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:hope_nest/models/advert.dart';
+import 'package:hope_nest/repository/repository.dart';
+import 'package:hope_nest/services/database/base/advert_db_base.dart';
+import 'package:hope_nest/util/enum/app_state.dart';
+import 'package:hope_nest/util/init/service_locator.dart';
+
+class AdvertVM with ChangeNotifier implements AdvertMethods {
+  AppState _state = AppState.IDLE;
+  final Repository _repository = serviceLocator<Repository>();
+  int limit = 10;
+
+  AppState get state => _state;
+
+  set state(AppState value) {
+    _state = value;
+    notifyListeners();
+  }
+
+  @override
+  Stream<List<Advert>>? getAdverts() {
+    return _repository.getAdverts();
+  }
+}
