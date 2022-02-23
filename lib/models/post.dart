476366733c2862808daaf5c886996939cfb13709
HakkiAkut <hakut37@gmail.com>
2022-02-24 01:50:01 +0300

    Model :: Post : date and url attribute added

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,15 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Post {
   String id;
   String uid;
   String? description;
   String? title;
+  Timestamp date;
+  String? url;
 
-  Post({required this.id, required this.uid, this.description, this.title});
+  Post(
+      {required this.id,
+      required this.uid,
+      required this.date,
+      this.description,
+      this.url,
+      this.title});
 
   Map<String, dynamic> toMap() {
     return {
       "id": id,
       "userId": uid,
+      "date": date,
+      "url": url,
       "description": description,
       "name": title,
     };
@@ -19,6 +31,8 @@ class Post {
     return Post(
       id: map['id'] as String,
       uid: map['userId'] as String,
+      date: map['date'] as Timestamp,
+      url: map['url'] as String,
       description: map['description'] as String,
       title: map['title'] as String,
     );
