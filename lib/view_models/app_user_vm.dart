eddc1f41a72046fe25ee9ada883a298f5e40f0a7
HakkiAkut <hakut37@gmail.com>
2022-02-24 23:49:53 +0300

    View Model :: AppUser : Comment methods added

--- lib/view_models/app_user_vm.dart
+++ lib/view_models/app_user_vm.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:hope_nest/models/app_user.dart';
+import 'package:hope_nest/models/comment.dart';
 import 'package:hope_nest/repository/repository.dart';
 import 'package:hope_nest/services/auth/base/auth_base.dart';
+import 'package:hope_nest/services/database/base/comments_db_base.dart';
 import 'package:hope_nest/services/database/base/user_db_base.dart';
 import 'package:hope_nest/util/enum/app_state.dart';
 import 'package:hope_nest/util/enum/login_state.dart';
 import 'package:hope_nest/util/init/service_locator.dart';
 
-class AppUserVM with ChangeNotifier implements AuthMethods, UserMethods {
+class AppUserVM
+    with ChangeNotifier
+    implements AuthMethods, UserMethods, CommentMethods {
   AppState _state = AppState.IDLE;
   LoginState _loginState = LoginState.SIGNIN;
 
   final Repository _repository = serviceLocator<Repository>();
   AppUser? _appUser;
   AppUser? _advertOwner;
+  List<Comment>? _comments;
 
   AppUser? get appUser => _appUser;
+
   AppUser? get advertOwner => _advertOwner;
+
+  List<Comment>? get comments => _comments;
+
   AppState get state => _state;
+
   LoginState get loginState => _loginState;
 
   AppUserVM() {
@@ -97,16 +107,29 @@ class AppUserVM with ChangeNotifier implements AuthMethods, UserMethods {
   }
 
   void getOwner(String id) async {
-    print("get owner start");
-    if(_advertOwner == null || _advertOwner!.uid != id){
+    if (_advertOwner == null || _advertOwner!.uid != id) {
       _advertOwner = await getUser(id: id);
     }
-    print("get owner end");
-    print(_advertOwner!.uid);
+  }
+
+  void getComment(String pid) async {
+    if (_comments == null || _comments!.first.pid != pid) {
+      _comments = await getCommentByPID(pid: pid);
+    }
   }
 
   @override
   Future<bool?> setUser({required AppUser appUser}) async {
     return await _repository.setUser(appUser: appUser);
+  }
+
+  @override
+  Future<List<Comment>?>? getCommentByPID({required String pid}) async {
+    try {
+      state = AppState.BUSY;
+      return await _repository.getCommentByPID(pid: pid);
+    } finally {
+      state = AppState.IDLE;
+    }
   }
 }
