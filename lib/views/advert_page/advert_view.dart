70833e3029a82797f7be97cf573b5298acacea36
HakkiAkut <hakut37@gmail.com>
2022-02-24 15:28:02 +0300

    View :: Advert : initial page created

---
+++ lib/views/advert_page/advert_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hope_nest/models/advert.dart';
+
+class AdvertView extends StatefulWidget {
+  final Advert advert;
+
+  const AdvertView({Key? key, required this.advert}) : super(key: key);
+
+  @override
+  _AdvertViewState createState() => _AdvertViewState();
+}
+
+class _AdvertViewState extends State<AdvertView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
