4b03d616e6ab6d6928c93bd64655b4d00d476acd
HakkiAkut <hakut37@gmail.com>
2022-02-25 19:05:39 +0300

    View :: Components : Owner info tile created

---
+++ lib/views/components/owner_info_tile/owner_info_tile.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:hope_nest/models/app_user.dart';
+import 'package:hope_nest/util/constants/navigation_constants.dart';
+
+class OwnerInfoTile extends StatelessWidget {
+  final AppUser appUser;
+
+  const OwnerInfoTile({Key? key, required this.appUser}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: appUser.image != "" ? Image.network(appUser.image!,
+        ): Image.asset("assets/user/user.png"),
+      ),
+      title: Text(appUser.name! + appUser.surname!),
+      onTap: () {
+        Navigator.pushNamed(context, NavigationConstants.PROFILE,
+            arguments: appUser);
+      },
+    );
+  }
+}
