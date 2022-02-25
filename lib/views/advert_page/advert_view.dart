40cafdd639fde2563bd7ac9f0a439fecad56cefc
HakkiAkut <hakut37@gmail.com>
2022-02-25 19:05:00 +0300

    View :: Advert View : UI update

--- lib/views/advert_page/advert_view.dart
+++ lib/views/advert_page/advert_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hope_nest/models/advert.dart';
 import 'package:hope_nest/util/enum/app_state.dart';
+import 'package:hope_nest/util/methods/dynamic_size.dart';
 import 'package:hope_nest/view_models/app_user_vm.dart';
+import 'package:hope_nest/views/components/advert_info_container/advert_info_container.dart';
+import 'package:hope_nest/views/components/owner_info_tile/owner_info_tile.dart';
 import 'package:provider/provider.dart';
 
 class AdvertView extends StatefulWidget {
@@ -21,12 +24,34 @@ class _AdvertViewState extends State<AdvertView> {
 
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: const Text("Return Back"),
       ),
-      body: Center(
-          child: _appUserVM.state == AppState.BUSY
+      body: Column(
+        children: [
+          Container(
+            width: DynamicSize.width(context, 1),
+            height: DynamicSize.height(context, 0.35),
+            color: Colors.orangeAccent.shade200,
+            child: Image.network(widget.advert.url),
+          ),
+          Container(
+            width: DynamicSize.width(context, 0.93),
+            height: DynamicSize.height(context, 0.11),
+            child: Text(
+              widget.advert.description!,
+              style: TextStyle(color: Colors.grey.shade600),
+            ),
+          ),
+          AdvertInfoContainer(advert: widget.advert),
+          SizedBox(
+            height: DynamicSize.height(context, 0.03),
+          ),
+          _appUserVM.state == AppState.BUSY
               ? const CircularProgressIndicator()
-              : Text(_appUserVM.advertOwner!.uid)),
+              : OwnerInfoTile(appUser: _appUserVM.advertOwner!)
+        ],
+      ),
     );
   }
 }
