20f670b84c98aa82b678ad65a0cbc836ebba924f
HakkiAkut <hakut37@gmail.com>
2022-02-27 02:32:30 +0300

    View :: Profile : UI Update

--- lib/views/profile/profile_page.dart
+++ lib/views/profile/profile_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hope_nest/util/enum/user_type.dart';
+import 'package:hope_nest/view_models/app_user_vm.dart';
 import 'package:hope_nest/views/components/app_bar/app_bar.dart';
+import 'package:hope_nest/views/components/image_container/image_container.dart';
+import 'package:provider/provider.dart';
 
 class ProfilePage extends StatefulWidget {
   final UserType userType;
@@ -14,8 +17,31 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(),
+    final _appUserVM = Provider.of<AppUserVM>(context);
+
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      body: Container(
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ImageContainer(
+                url: widget.userType == UserType.mainUser
+                    ? _appUserVM.appUser!.image!
+                    : (widget.userType == UserType.postOwner
+                        ? _appUserVM.postOwner!.image!
+                        : _appUserVM.advertOwner!.image!),
+                color: Colors.white),
+            Container(
+              child: Text(widget.userType == UserType.mainUser
+                  ? _appUserVM.appUser!.name!
+                  : (widget.userType == UserType.postOwner
+                      ? _appUserVM.postOwner!.name!
+                      : _appUserVM.advertOwner!.name!)),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
