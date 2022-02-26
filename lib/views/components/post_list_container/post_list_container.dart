f2b4499303fe373d08f274ab214ba3219174abc5
HakkiAkut <hakut37@gmail.com>
2022-02-26 14:22:30 +0300

    View :: Components : post list container created

---
+++ lib/views/components/post_list_container/post_list_container.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:hope_nest/models/post.dart';
+import 'package:hope_nest/util/constants/navigation_constants.dart';
+import 'package:hope_nest/util/methods/dynamic_size.dart';
+import 'package:intl/intl.dart';
+
+class PostListContainer extends StatelessWidget {
+  final Post post;
+
+  const PostListContainer({Key? key, required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        child: Container(
+          margin: const EdgeInsets.all(9),
+          height: DynamicSize.height(context, 0.08),
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.orangeAccent),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(width: 1, color: Colors.orangeAccent),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.network(
+                        post.url!,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Text("${post.title}"),
+                ],
+              ),
+              Text(
+                DateFormat('dd/MM/yyyy').format(post.date.toDate()),
+              ),
+            ],
+          ),
+        ),
+        onTap: () {
+          Navigator.pushNamed(context, NavigationConstants.POST_PAGE,
+              arguments: post);
+        });
+  }
+}
