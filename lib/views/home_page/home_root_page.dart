141a31070b4dd1bdd7c153d494bf082fd34f2247
HakkiAkut <hakut37@gmail.com>
2022-02-27 17:07:36 +0300

    View :: Components : User profile image added

--- lib/views/home_page/home_root_page.dart
+++ lib/views/home_page/home_root_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hope_nest/util/constants/navigation_constants.dart';
 import 'package:hope_nest/util/constants/palette.dart';
+import 'package:hope_nest/util/enum/user_type.dart';
 import 'package:hope_nest/util/methods/dynamic_size.dart';
 import 'package:hope_nest/view_models/app_user_vm.dart';
 import 'package:hope_nest/views/components/navigation_bar/navigation_bar.dart';
+import 'package:hope_nest/views/components/owner_info_tile/circular_user_image.dart';
 import 'package:hope_nest/views/home_page/blog/blog_provider.dart';
 import 'package:hope_nest/views/home_page/home/home_provider.dart';
 import 'package:hope_nest/views/home_page/messages/messages_provider.dart';
@@ -41,9 +44,21 @@ class _HomeRootPageState extends State<HomeRootPage> {
                     child: const NavigationBar(),
                   ),
                 ),
-                const Expanded(
+                Expanded(
                   flex: 1,
-                  child: Icon(Icons.add),
+                  child: GestureDetector(
+                    child: CircularUserImage(
+                      image: _appUserVM.appUser!.image != ""
+                          ? Image.network(
+                              _appUserVM.appUser!.image!,
+                            )
+                          : Image.asset("assets/user/user.png"),
+                    ),
+                    onTap: () {
+                      Navigator.pushNamed(context, NavigationConstants.PROFILE,
+                          arguments: UserType.mainUser);
+                    },
+                  ),
                 )
               ],
             ),
