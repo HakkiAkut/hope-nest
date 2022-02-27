dc7cdbc83345def0bcc53e9779173d7464bef614
HakkiAkut <hakut37@gmail.com>
2022-02-28 02:49:58 +0300

    View :: Post View : UI update

--- lib/views/post_page/post_view.dart
+++ lib/views/post_page/post_view.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hope_nest/models/post.dart';
+import 'package:hope_nest/util/constants/palette.dart';
 import 'package:hope_nest/util/enum/app_state.dart';
 import 'package:hope_nest/util/enum/user_type.dart';
 import 'package:hope_nest/util/methods/dynamic_size.dart';
 import 'package:hope_nest/view_models/app_user_vm.dart';
 import 'package:hope_nest/views/components/app_bar/app_bar.dart';
 import 'package:hope_nest/views/components/comment_list/comment_list.dart';
+import 'package:hope_nest/views/components/image_container/image_container.dart';
 import 'package:hope_nest/views/components/owner_info_tile/owner_info_tile.dart';
+import 'package:hope_nest/views/components/styles/background_style.dart';
 import 'package:provider/provider.dart';
 
 class PostView extends StatefulWidget {
@@ -24,54 +27,61 @@ class _PostViewState extends State<PostView> {
   Widget build(BuildContext context) {
     final _appUserVM = Provider.of<AppUserVM>(context);
     _appUserVM.getCommentAndOwner(pid: widget.post.id, uid: widget.post.uid);
-    return Scaffold(
-      appBar: const CustomAppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              width: DynamicSize.width(context, 0.83),
-              height: DynamicSize.height(context, 0.35),
-              color: Colors.white,
-              child: Image.network(widget.post.url!),
-            ),
-            Text(
-              widget.post.title!,
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Event ID:  ",
-                  style: TextStyle(color: Colors.black38, fontSize: 12),
-                ),
-                Text(
-                  widget.post.id!,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 12),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: DynamicSize.height(context, 0.03),
-            ),
-            _appUserVM.state == AppState.BUSY
-                ? const CircularProgressIndicator()
-                : OwnerInfoTile(
-                    appUser: _appUserVM.postOwner!,
-                    userType: UserType.postOwner,
-                  ),
-            Container(
-              width: DynamicSize.width(context, 0.93),
-              height: DynamicSize.height(context, 0.11),
-              child: Text(
-                widget.post.description!,
-                style: TextStyle(color: Colors.grey.shade600),
+    return Container(
+      decoration: backgroundStyle,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: const CustomAppBar(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ImageContainer(
+                url: widget.post.url!,
+                color: Palette.BACKGROUND,
               ),
-            ),
-            const CommentList(),
-          ],
+              SizedBox(
+                height: DynamicSize.height(context, 0.016),
+              ),
+              Text(
+                widget.post.title!,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Event ID:  ",
+                    style: TextStyle(color: Colors.black38, fontSize: 10),
+                  ),
+                  Text(
+                    widget.post.id!,
+                    style: const TextStyle(fontSize: 10),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: DynamicSize.height(context, 0.012),
+              ),
+              _appUserVM.state == AppState.BUSY
+                  ? const CircularProgressIndicator()
+                  : OwnerInfoTile(
+                      appUser: _appUserVM.postOwner!,
+                      userType: UserType.postOwner,
+                    ),
+              Container(
+                width: DynamicSize.width(context, 0.93),
+                child: Text(
+                  widget.post.description!,
+                  style: TextStyle(color: Colors.grey.shade600, fontSize: 11),
+                ),
+              ),
+              SizedBox(
+                height: DynamicSize.height(context, 0.012),
+              ),
+              const CommentList(),
+            ],
+          ),
         ),
       ),
     );
