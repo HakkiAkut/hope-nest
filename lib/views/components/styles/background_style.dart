497bcca23d745c2d9177e9f29b22b06b9509f31b
HakkiAkut <hakut37@gmail.com>
2022-02-28 02:43:00 +0300

    Model :: Component : background style created

---
+++ lib/views/components/styles/background_style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const backgroundStyle = BoxDecoration(
+  image: DecorationImage(
+      image: AssetImage("assets/login/background2.jpg"),
+      fit: BoxFit.cover),
+);
