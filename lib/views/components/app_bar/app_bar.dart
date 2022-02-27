66f63578fb2499c1c64d5bf7fdcffa5a21406d76
HakkiAkut <hakut37@gmail.com>
2022-02-27 23:38:47 +0300

    View :: Components : Custom app bar changed to Container due to UI update

--- lib/views/components/app_bar/app_bar.dart
+++ lib/views/components/app_bar/app_bar.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hope_nest/util/constants/palette.dart';
+import 'package:hope_nest/util/methods/dynamic_size.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final Widget? actionWidget;
@@ -7,20 +10,30 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      leading: GestureDetector(
-        child: const Icon(Icons.arrow_back_ios),
-        onTap: () {
-          Navigator.pop(context);
-        },
+    return Container(
+      margin: EdgeInsets.only(top: DynamicSize.height(context, 0.03),right: DynamicSize.width(context, 0.04),left: DynamicSize.width(context, 0.04)),
+      color: Colors.white,
+      child: Row(
+        children: [
+          GestureDetector(
+            child: const Icon(Icons.arrow_back_ios,
+                color: Palette.MAIN_COLOR_BLUE),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          const Text(
+            "Return Back",
+            style: TextStyle(fontSize: 20, color: Palette.MAIN_COLOR_BLUE),
+          ),
+          isNull(),
+        ],
       ),
-      title: const Text("Return Back"),
-      actions: <Widget>[isNull()],
     );
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => Size.fromHeight(40);
 
   Widget isNull() {
     if (actionWidget == null) {
