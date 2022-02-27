f24554c1cef5f016a4fc6942a606315c843be6c6
HakkiAkut <hakut37@gmail.com>
2022-02-28 02:49:07 +0300

    View Model :: Component : Image Container, adding radius feature added

--- lib/views/components/image_container/image_container.dart
+++ lib/views/components/image_container/image_container.dart
@@ -4,20 +4,25 @@ import 'package:hope_nest/util/methods/dynamic_size.dart';
 class ImageContainer extends StatelessWidget {
   final String url;
   final String? alt;
+  final double? radius;
   final Color color;
 
   const ImageContainer(
-      {Key? key, required this.url, this.alt, required this.color})
+      {Key? key, required this.url, this.alt, required this.color, this.radius})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: DynamicSize.width(context, 1),
-      height: DynamicSize.height(context, 0.35),
+      height: DynamicSize.height(context, 0.33),
       color: color,
-      child:
-          url != "" ? Image.network(url) : Image.asset("assets/user/user.png"),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(radius ?? 0),
+        child: url != ""
+            ? Image.network(url)
+            : Image.asset("assets/user/user.png"),
+      ),
     );
   }
 }
