e02064f15c47c978d08f112661209a19a810b98b
HakkiAkut <hakut37@gmail.com>
2022-02-28 02:43:33 +0300

    Model :: Component : Circular user image fixed height given

--- lib/views/components/owner_info_tile/circular_user_image.dart
+++ lib/views/components/owner_info_tile/circular_user_image.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hope_nest/util/methods/dynamic_size.dart';
 
 class CircularUserImage extends StatelessWidget {
   final Image image;
@@ -7,9 +8,12 @@ class CircularUserImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(50.0),
-      child: image,
+    return Container(
+      height: DynamicSize.height(context, 0.065),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(50.0),
+        child: image,
+      ),
     );
   }
 }
