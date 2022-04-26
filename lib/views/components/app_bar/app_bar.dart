399cf5267f485a2fc1dde46586d5da5922c8b9e1
HakkiAkut <hakut37@gmail.com>
2022-04-26 21:36:09 +0300

    view :: components : app bar updated

--- lib/views/components/app_bar/app_bar.dart
+++ lib/views/components/app_bar/app_bar.dart
@@ -5,26 +5,37 @@ import 'package:hope_nest/util/methods/dynamic_size.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final Widget? actionWidget;
+  final String text;
 
-  const CustomAppBar({Key? key, this.actionWidget}) : super(key: key);
+  const CustomAppBar({Key? key, this.actionWidget, required this.text})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: DynamicSize.height(context, 0.03),right: DynamicSize.width(context, 0.04),left: DynamicSize.width(context, 0.04)),
+      margin: EdgeInsets.only(
+          top: DynamicSize.height(context, 0.03),
+          right: DynamicSize.width(context, 0.04),
+          left: DynamicSize.width(context, 0.04)),
       color: Colors.white,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          GestureDetector(
-            child: const Icon(Icons.arrow_back_ios,
-                color: Palette.MAIN_COLOR_BLUE),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          const Text(
-            "Return Back",
-            style: TextStyle(fontSize: 20, color: Palette.MAIN_COLOR_BLUE),
+          Row(
+            children: [
+              GestureDetector(
+                child: const Icon(Icons.arrow_back_ios,
+                    color: Palette.MAIN_COLOR_BLUE),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              Text(
+                text,
+                style: const TextStyle(
+                    fontSize: 20, color: Palette.MAIN_COLOR_BLUE),
+              )
+            ],
           ),
           isNull(),
         ],
