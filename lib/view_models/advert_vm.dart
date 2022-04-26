76270a2ce625dd375bbdef04f3414328de40fa16
HakkiAkut <hakut37@gmail.com>
2022-04-26 21:36:50 +0300

    View Model :: AdvertVM : search advert methods created

--- lib/view_models/advert_vm.dart
+++ lib/view_models/advert_vm.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:hope_nest/models/advert.dart';
+import 'package:hope_nest/models/search.dart';
 import 'package:hope_nest/repository/repository.dart';
 import 'package:hope_nest/services/database/base/advert_db_base.dart';
 import 'package:hope_nest/services/storage/base/storage_base.dart';
@@ -12,17 +13,35 @@ class AdvertVM with ChangeNotifier implements AdvertMethods, StorageMethods {
   AppState _state = AppState.IDLE;
   final Repository _repository = serviceLocator<Repository>();
   int limit = 10;
+  SearchAdvert _searchAdvert = SearchAdvert();
 
   AppState get state => _state;
+
+  SearchAdvert get searchAdvert => _searchAdvert;
 
   set state(AppState value) {
     _state = value;
     notifyListeners();
   }
 
+  set searchAdvert(SearchAdvert searchAdvert) {
+    _searchAdvert = searchAdvert;
+    print(searchAdvert.location);
+    notifyListeners();
+  }
+
+  bool isSearchAdvertNull() {
+    return (_searchAdvert.location == null || _searchAdvert.location == '')
+        ? true
+        : false;
+  }
+
   @override
   Stream<List<Advert>>? getAdverts() {
-    return _repository.getAdverts();
+    print("getadverts");
+    return isSearchAdvertNull()
+        ? _repository.getAdverts()
+        : _repository.getFilteredAdverts(searchAdvert: _searchAdvert);
   }
 
   @override
