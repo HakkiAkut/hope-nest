b325f5bf7a964ba73f6df19ef3b55a9bf33c0d65
HakkiAkut <hakut37@gmail.com>
2022-04-26 21:23:22 +0300

    View :: Component : Custom floating action button onPressed added

--- lib/views/components/custom_floating_action_button/custom_floating_action_button.dart
+++ lib/views/components/custom_floating_action_button/custom_floating_action_button.dart
@@ -1,25 +1,24 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hope_nest/util/constants/palette.dart';
 import 'package:hope_nest/view_models/app_user_vm.dart';
-import 'package:hope_nest/views/components/send_advert/send_advert.dart';
 import 'package:provider/provider.dart';
 
 class CustomFloatingActionButton extends StatelessWidget {
-  const CustomFloatingActionButton({Key? key}) : super(key: key);
+  final VoidCallback onPressed;
+  final String text;
+
+  const CustomFloatingActionButton({Key? key, required this.onPressed, required this.text})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final _appUserVM = Provider.of<AppUserVM>(context);
+    //final _appUserVM = Provider.of<AppUserVM>(context);
     return FloatingActionButton.extended(
       backgroundColor: Palette.MAIN_COLOR_BLUE,
-      onPressed: () {
-        print("Button is pressed.");
-        SendAdvert().dialog(context: context, appUser: _appUserVM.appUser!);
-      },
-      label: const Text(
-        "Share new Friend",
-        style: TextStyle(color: Palette.BACKGROUND),
+      onPressed: () => onPressed.call(),
+      label:  Text(
+        text,
+        style: const TextStyle(color: Palette.BACKGROUND),
       ),
     );
   }
