ac01c36dd714cb29a665a69685517d877cf2231c
Gizem Muratoğlu <58625642+gizemmuratoglu@users.noreply.github.com>
2022-05-11 13:06:32 +0300

    VIEW : MESSAGES ::messages_provider :has been created

--- lib/views/home_page/messages/messages_provider.dart
+++ lib/views/home_page/messages/messages_provider.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hope_nest/models/chatroom.dart';
+import 'package:hope_nest/view_models/chatRoom_vm.dart';
+import 'package:hope_nest/views/home_page/blog/blog_view.dart';
+import 'package:hope_nest/views/home_page/messages/messages_view.dart';
+import 'package:provider/provider.dart';
+
+import '../../../view_models/app_user_vm.dart';
 
 class MessagesProvider extends StatelessWidget {
   const MessagesProvider({Key? key}) : super(key: key);
@@ -6,9 +13,12 @@ class MessagesProvider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO Message's Provider and View Page
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Container(child: Text("Messages"),),
+    final _appUserVM = Provider.of<AppUserVM>(context);
+    return StreamProvider<List<ChatRoom>>.value(
+    value: ChatRoomVM().getChatRoom(id: _appUserVM.appUser!.uid),
+    initialData: const [],
+    child: const MessagesView(),
+    updateShouldNotify: (prev, now) => true,
     );
   }
 }
