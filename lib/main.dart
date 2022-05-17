a7256b030bdeca81097c87ee2ba9068ec816c288
gizemmuratoglu <58625642+gizemmuratoglu@users.noreply.github.com>
2022-05-17 16:04:27 +0300

    message_vm is added in changeNotifierProvider.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:hope_nest/util/init/service_locator.dart';
 import 'package:hope_nest/view_models/advert_vm.dart';
 import 'package:hope_nest/view_models/app_user_vm.dart';
 import 'package:hope_nest/view_models/chatRoom_vm.dart';
+import 'package:hope_nest/view_models/message_vm.dart';
 import 'package:hope_nest/view_models/report_vm.dart';
 import 'package:hope_nest/views/root.dart';
 import 'package:provider/provider.dart';
@@ -35,6 +36,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (context) => ChatRoomVM(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => MessageVM(),
         ),
       ],
       child: MaterialApp(
