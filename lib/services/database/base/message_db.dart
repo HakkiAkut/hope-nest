ae9d350e7a3905ce2068fafead50d60486747632
gizemmuratoglu <58625642+gizemmuratoglu@users.noreply.github.com>
2022-05-17 11:18:13 +0300

    SERVICE::DATABASE::BASE:: message_db is changed.

--- lib/services/database/base/message_db.dart
+++ lib/services/database/base/message_db.dart
@@ -1,6 +1,6 @@
 import 'package:hope_nest/models/chatroom.dart';
 import 'package:hope_nest/models/messages.dart';
 
-abstract class ChatRoomMethod {
-  Stream<List<Messages>>? getMessage({required String id,required String Cid}); // cid: chatroom id,  id:: user id
+abstract class MessageMethod {
+  Stream<List<Messages>>? getMessage({required String cid}); // cid: chatroom id
 }
